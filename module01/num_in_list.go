65df9098093010959ce26cb5d684353614d8e709
Konstantin Kolgushkin <konstantin.kolgushkin@gmail.com>
2021-04-13 00:28:09 +0300

    NumInList. Added nil list check

--- module01/num_in_list.go
+++ module01/num_in_list.go
@@ -3,6 +3,11 @@ package module01
 // NumInList will return true if num is in the
 // list slice, and false otherwise.
 func NumInList(list []int, num int) bool {
+
+	if list == nil {
+		return false
+	}
+
 	for _, x := range list {
 		if num == x {
 			return true
