1cf11a29452ce429bda9baa2ace753731e57c8dc
Konstantin Kolgushkin <konstantin.kolgushkin@gmail.com>
2021-04-16 11:52:01 +0300

    Reverse a string

--- module01/reverse.go
+++ module01/reverse.go
@@ -6,6 +6,10 @@ package module01
 //   Reverse("cat") => "tac"
 //   Reverse("alphabet") => "tebahpla"
 //
-func Reverse(word string) string {
-	return ""
+func Reverse(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
 }
