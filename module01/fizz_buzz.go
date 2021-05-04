7f338c3f99d4a569196f1416339b7817f01f510b
Konstantin Kolgushkin <konstantin.kolgushkin@gmail.com>
2021-05-04 18:04:01 +0300

    FizzBuzz space separator

--- module01/fizz_buzz.go
+++ module01/fizz_buzz.go
@@ -1,14 +1,35 @@
 package module01
 
-// FizzBuzz will print out all of the numbers
+import "fmt"
+
+// FizzBuzz prints out all of the numbers
 // from 1 to N replacing any divisible by 3
 // with "Fizz", and divisible by 5 with "Buzz",
-// and any divisible by both with "Fizz Buzz".
+// and any divisible by both with "FizzBuzz".
 //
 // Note: The test for this is a little
 // complicated so that you can just use the
 // `fmt` package and print to standard out.
 // I wouldn't normally recommend this, but did
 // it here to make life easier for beginners.
+
 func FizzBuzz(n int) {
+
+	for i := 1; i <= n; i++ {
+
+		var isFizz = (i%3 == 0)
+		var isBuzz = (i%5 == 0)
+		var isFizzBuzz = (isFizz && isBuzz)
+
+		if isFizzBuzz {
+			fmt.Print("FizzBuzz")
+		} else if isFizz {
+			fmt.Print("Fizz")
+		} else if isBuzz {
+			fmt.Print("Buzz")
+		} else {
+			fmt.Print(i)
+		}
+		fmt.Print(" ")
+	}
 }
