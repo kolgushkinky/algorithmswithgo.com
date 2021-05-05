52b1c083197d48679445a7e130b2a7bf12bbc6f3
Konstantin Kolgushkin <konstantin.kolgushkin@gmail.com>
2021-05-06 00:01:55 +0300

    Base to Decimal

--- module01/base_to_dec.go
+++ module01/base_to_dec.go
@@ -1,5 +1,7 @@
 package module01
 
+import "math"
+
 // BaseToDec takes in a number and the base it is currently
 // in and returns the decimal equivalent as an integer.
 //
@@ -9,5 +11,19 @@ package module01
 //   BaseToDec("1110", 2) => 14
 //
 func BaseToDec(value string, base int) int {
-	return 0
+	//	var digits string = "0123456789ABCDEF"
+
+	var res int
+	l := len(value)
+
+	for i, r := range value {
+		var digit int
+		if r < 65 {
+			digit = int(r - '0')
+		} else {
+			digit = int(r - 55)
+		}
+		res += digit * int(math.Pow(float64(base), float64(l-i-1)))
+	}
+	return res
 }
